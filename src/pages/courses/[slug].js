50f67e748a2ca50a28e65fba23c88d9db37c23f8
ussraa <92607242+youssra-ghninou@users.noreply.github.com>
2023-02-07 02:48:31 +0100

    feat: profile buttons border left

--- src/pages/courses/[slug].js
+++ src/pages/courses/[slug].js
@@ -1,3 +1,6 @@
+import DesktopNav from '@/components/Home/DesktopNav'
+import Footer from '@/components/Home/Footer'
+import Head from 'next/head'
 import Link from 'next/link'
 import { getCourseBySlug, getCourses } from '../../utils/db'
 
@@ -5,19 +8,32 @@ const CoursePage = ({ course: { title, price, lessons, description } }) => {
   const isFree = price === 0
 
   return (
-    <div className='text-white'>
-      <h2 className='text-3xl'>{title}</h2>
-      <h2 className='text-3xl'>{description}</h2>
-      <ul className='p-2'>
-        {lessons.map((lesson) => (
-          <li key={lesson.id}>
-            <Link href={`/courses/lesson/${lesson.slug}`}>
-              {lesson.id}. {lesson.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Head>
+        <title>Skills Builder</title>
+        <meta name='description' content='Skills builder platform' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/logo.png' />
+      </Head>
+      <main className='mx-auto flex max-w-[1440px] flex-col gap-24 bg-primary p-10'>
+        <DesktopNav />
+        <div className='text-text'>
+          <h2 className='text-3xl'>{title}</h2>
+          <h2 className='text-3xl'>{description}</h2>
+          <ul className='p-2'>
+            {lessons.map((lesson) => (
+              <li key={lesson.id}>
+                <Link href={`/courses/lesson/${lesson.slug}`}>
+                  {lesson.id}. {lesson.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <Footer />
+      </main>
+    </>
   )
 }
 
