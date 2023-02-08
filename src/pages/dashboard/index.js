134d63ac17587dd27457310318d9488482a1e859
aimdexter <draimade@gmail.com>
2023-02-08 05:22:17 +0100

    fix: fixed dashboard for non logged in users

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -2,14 +2,18 @@ import CourseCard from '@/components/Home/CourseCard'
 import DashboardLayout from '@/dashboard/layout'
 import DashboardProvider from '@/dashboard/provider/context'
 import { getCourses } from '@/utils/db'
+import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 
 export default function Page({ courses }) {
   return (
-    <div className='flex h-full flex-col gap-5 self-center rounded-xl bg-primary pt-5'>
-      <div className='self-center'>
-        <CourseCard name={'Your '} courses={courses} />
+    <>
+      <div className='flex h-full flex-col gap-5 self-center rounded-xl bg-primary pt-5'>
+        <div className='self-center'>
+          <CourseCard name={'Your '} courses={courses} />
+        </div>
       </div>
-    </div>
+      <CourseCard name={'Your '} courses={courses} />
+    </>
   )
 }
 
@@ -21,12 +25,14 @@ Page.getLayout = function getLayout(page) {
   )
 }
 
-export const getStaticProps = async () => {
-  const courses = await getCourses()
+export const getServerSideProps = withPageAuthRequired({
+  async getServerSideProps() {
+    const courses = await getCourses()
 
-  return {
-    props: {
-      courses: JSON.parse(JSON.stringify(courses)),
-    },
-  }
-}
+    return {
+      props: {
+        courses: JSON.parse(JSON.stringify(courses)),
+      },
+    }
+  },
+})
